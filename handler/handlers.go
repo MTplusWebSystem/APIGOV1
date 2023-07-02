610ecb1d896a63710e4dd54ee239d588dc56c97a
MTplusWebSystem <MTplusWebSystem@mail.com>
2023-07-01 22:43:18 -0400

    Criando todos Handlers

---
+++ handler/handlers.go
@@ -0,0 +1,26 @@
+package handler
+
+func CreateHandle(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"opening": "GET 200",
+		})}
+		
+func ShowHandle(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"opening": "GET 200",
+		})}
+		
+func DeleteHandle(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"opening": "GET 200",
+		})}
+
+func UpdateHandle(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"opening": "GET 200",
+		})}
+		
+func ListAllHandle(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"opening": "GET 200",
+		})}
--- router/controllers.go
+++ router/controllers.go
@@ -6,8 +6,26 @@ func msg(){
 }
 func start_control(controller *gin.Engine){
   v1 := controller.Group("/api/v1")
+  
   v1.GET("opening",func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"opening": "GET 200",
 		})})
+		
+	v1.POST("opening",func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"opening": "POST 200",
+		})})
+	
+	v1.DELETE("opening",func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"opening": "DELETE 200",
+		})})
+		
+	v1.PUT("opening",func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"opening": "PUT 200",
+		})})
+		
+	v1.GET("openings",)
 }
