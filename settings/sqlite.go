3753ad06a0be82f060b467d79e2816ee2f4a0336
MTplusWebSystem <MTplusWebSystem@mail.com>
2023-07-04 05:56:09 -0400

    Montando a estrutura

--- settings/sqlite.go
+++ settings/sqlite.go
@@ -1,10 +1,25 @@
 package settings 
-
+/*
 import (
   "gorm.io/gorm"
+  "gorm.io/driver/sqlite"
   )
+
+type Company struct {
+  gorm.Model
+  Code  string
+  Price uint
+}
 
 func SetSQLite()(*gorm.DB,error){
   logger := Logs("sqlite")
+  dbPath:= "./db/main.db"
   
-}
+  
+  db, err:= gorm.Open(sqlite.Open(dbPath,&gorm.Config{}))
+  
+  if err !=nil{
+    logger.Errof("SQLITE ERRO %v",erro)
+    return nil, err
+  }
+}*/
